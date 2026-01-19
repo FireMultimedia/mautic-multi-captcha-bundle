11070512fc16df0ce8d587f1f7304802016a513f
FireMultimedia <support@firemultimedia.nl>
2026-01-19 16:10:14 +0100

    Bump version number

--- Config/config.php
+++ Config/config.php
@@ -66,7 +66,7 @@ switch(true) {
 return [
     "name"        => "MultiCAPTCHA",
     "description" => "Enables Google's reCAPTCHA, hCaptcha, and Cloudflare Turnstile integration for Mautic",
-    "version"     => "1.0.3",
+    "version"     => "1.0.4",
     "author"      => "FireMultimedia B.V.",
 
     "routes" => [
